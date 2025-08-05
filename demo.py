a995d9bd22fa2cedef17f05b14240d6023f84c8a
Tiff Wu <33282030+gift-is-coding@users.noreply.github.com>
2025-08-05 15:29:46 +0000

    delete

--- demo.py
+++ demo.py
@@ -36,8 +36,4 @@ if __name__ == "__main__":
     random_float = random.uniform(10, 20)
     print("Random float between 10 and 20:", random_float)
 
-    # Generate a random integer between 50 and 150
-    random_int = random.randint(50, 150)
-    print("Random integer between 50 and 150:", random_int)
-    # Generate a random number from a normal distribution
-    normal_random = random.gauss(0, 1)
+
